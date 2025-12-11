5559a672e5dccf185a8b8dc5b96b163c6c658ac7
WEBTERR0R <artemshestvik@gmail.com>
2025-12-12 01:55:19 +0700

    Добавлено умножение

--- src/main.py
+++ src/main.py
@@ -3,4 +3,8 @@ print("Hello, World!")
 def addition(a, b):
     return a + b
 
+def multiplication(a, b):
+    return a * b
+
 print("Result of addition:", addition(5, 3))
+print("Result of multiplication:", multiplication(4, 3))
